24b634dbe806e2a16f59925b63bdc860bc6029e3
simranffdl-crypto <simranffdl@gmail.com>
2026-04-28 15:03:29 +0530

    mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-
+import "./App.css";
 import Footer from "./components/Footer";
-import AboutUs from "./components/About Us";
-import CallToAction from "./components/Call to action";
+import AboutUs from "./components/AboutUs";
+import CallToAction from "./components/CallToAction";
 import Clients from "./components/Clients";
-import ContactUs from "./components/Contact Us";
+import ContactUs from "./components/ContactUs";
 import Navigation from "./components/Navigation";
 import Portfolio from "./components/Portfolio";
 import Pricing from "./components/Pricing";
 import Process from "./components/Process";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
-import WhyChooseUs from "./components/Whychoose us";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 function App() {
   return (
-    <>
+    <div className="app-wrapper">
       <Navigation />
-      <AboutUs />
-      <Services />
-      <WhyChooseUs />
-      <Process />
-      <Portfolio />
-      <Pricing />
-      <Testimonials />
-      <Clients />
-      <CallToAction />
-      <ContactUs />
+      <main className="main-content">
+        <AboutUs />
+        <Services />
+        <WhyChooseUs />
+        <Process />
+        <Portfolio />
+        <Pricing />
+        <Testimonials />
+        <Clients />
+        <CallToAction />
+        <ContactUs />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
